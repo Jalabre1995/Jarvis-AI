7bf3d2680e7248a016a99accd3f3216e013265cc
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-10-30 15:39:07 -0400

    Added the window and calle it Jarvis and managed to have the query from wolframalpha to popup another window

--- pyda.py
+++ pyda.py
@@ -1,4 +1,4 @@
- Get access to wolfram Alpha, where it ask you something and give you an answer.
+# Get access to wolfram Alpha, where it ask you something and give you an answer.
 import wolframalpha
 client = wolframalpha.Client('V4L279-552P4V4JW3')
 
@@ -7,19 +7,17 @@ import PySimpleGUI as sg
 
 sg.theme('DarkPurple')
 # All the stuff inside your window.
-layout = [  [sg.Text('Enter a command'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
-
-# Create the Window
+layout = [[sg.Text('Enter a command'), sg.InputText()],[sg.Button('Ok'), sg.Button('Cancel')]]
 window = sg.Window('Jarvis', layout)
+
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    if event in (None, 'Cancel'):
         break
+    
     res = client.query(values[0])
-
-
-    print(next(res.results).text)
+    
+    sg.Popup(next(res.results).text)
 
 window.close()
